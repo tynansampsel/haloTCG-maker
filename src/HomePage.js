cd124fc31897920036f4ced2ba9731225aa86201
tynan Sampsel <tynansampselr@gmail.com>
2024-05-01 11:43:00 -0300

    added steps to mainpage

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,13 @@ import './css/App.css';
 function HomePage() {
 	return (
 		<div className="HomePage">
-			welcome
+			Welcome to HaloTCG Maker! Here is a quick guide how to use this
+			<ol>
+				<li>go to Manage and create a new card set or upload an existing card set.</li>
+				<li>go to Maker and either creating a new card or edit an existing card.</li>
+				<li>go to Generate to generate the cards</li>
+				<li>go to Manage and download your card set for future use</li>
+			</ol>
 		</div>
 	);
 }
